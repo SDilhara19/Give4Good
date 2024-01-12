20fe5caf0045ea62452863ad2b7237b38306b935
SDilhara19 <sdilharabandara@gmail.com>
2024-01-12 07:13:08 +0530

    Admin - functions controller

---
+++ app/views/Admin/V_Contributions.php
@@ -0,0 +1,38 @@
+<html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIVE4GOOD</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/styles/styles.css">
+  <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/styles/components-3/Admin_style.css"> -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9e9a03ae37.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <?php require APPROOT . '/views/includes/side-dash.php' ?>
+  <section class="home-section">
+  <?php require APPROOT . '/views/includes/header.php' ?>
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+      <span class="text">Drop Down Sidebar</span>
+      <main>
+        <h1>FAQ Page</h1>
+        <h1>FAQ Page</h1>
+        <h1>FAQ Page</h1>
+        <h1>FAQ Page</h1>
+        <h1>FAQ Page</h1>
+        <h1>FAQ Page</h1>
+    </main>
+    </div>
+  </section>
+
+
+</body>
+</html>
--- app/views/Index/V_Index.php
+++ app/views/Index/V_Index.php
@@ -14,6 +14,7 @@
 
 </head>
 <body>
+  
 <?php require APPROOT . '/views/includes/header.php' ?>
 
 <main>
