fd12eae70556eee123475d3b88e549fdbaba62ad
BhagyaLiyanage <bhagyaliyanage99@gmail.com>
2024-01-13 07:37:28 -0800

    Did some changes

--- app/views/Admin_Organisation/V_All_Super.php
+++ app/views/Admin_Organisation/V_All_Super.php
@@ -13,24 +13,7 @@
 <body>
   <?php require APPROOT . '/views/includes/admin-header.php' ?>
   <?php require APPROOT . '/views/includes/admin-dashboard.php' ?>
-    <a href="#" class="logo">
-      <img src="LogoBlack.png" alt="" class="LOGO-img">
-    </a>
-    <ul class="nav-links">
-      <li><a href="Admindashboard.html"><i class="fa-brands fa-windows"></i><p>Dashboard</p></a></li>
-      <li><a href="AdmindoneeIndividuals.html"><i class="fa-solid fa-people-group"></i><p>Individual Donees</p></a></li>
-      <li><a href="AdmindonorIndividuals.html"><i class="fa-solid fa-people-group"></i><p>Individual Donors</p></a></li>
-      <li><a href="AdmindonorOrganizations.html"><i class="fa-solid fa-people-roof"></i><p>Donor Organizations</p></a></li>
-      <li><a href="AdmindoneeOrganizations.html"><i class="fa-solid fa-people-roof"></i><p>Donee Organizations</p></a></li>
-      <li><a href="AdminFundraisers-active.html"><i class="fa-solid fa-table"></i><p>Fundraisers</p></a></li>
-      <li><a href="AdminDonations.html"><i class="fa-solid fa-table"></i><p>Donations</p></a></li>
-      <li><a href="AdminStories.html"><i class="fa-solid fa-table"></i><p>Stories</p></a></li>
-      <li><a href="AdminComplaigns.html"><i class="fa-regular fa-comments"></i><p>Complaigns</p></a></li>
-      <li><a href="AdminaddAdmin.html"><i class="fa-solid fa-user-plus"></i><p>Add an admin</p></a></li>
-      <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i><p>Log out</p></a></li>
-      <div class="active"></div>
-    </ul>
-  </div>
+    
   <div class="main--content">
     <div class="header--wrapper">
       <div class="header--title">
