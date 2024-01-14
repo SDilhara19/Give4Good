696882cf8365160c8d7e62892539c22d7fa899c4
Sanduni Dilhara <125456493+SDilhara19@users.noreply.github.com>
2024-01-14 06:18:51 +0530

    Update districts.js

--- public/js/districts.js
+++ public/js/districts.js
@@ -1,22 +1,26 @@
+ var districtOptions = document.querySelector("#district")
+   var province = selectedProvince.value
 
-function selectedProvince() {
-  var selectedProvince = document.querySelector("#province")
-  var districtOptions = document.querySelector("#district")
-  var province = selectedProvince.value
+   districtOptions.innerHTML = "";
+
+   if (province === "Western") {
+     addOption("Select district", "sede", districtOptions, true);
+     addOption("Colombo", "Colombo", districtOptions);
+     addOption("Gampaha", "Gampaha", districtOptions);
+     addOption("Kaluthara", "Kaluthara", districtOptions);
 
 
+   }
+   else if (province === "Central") {
 
-}
+     addOption("Kandy", "Kandy", districtOptions);
 
+   }
+
+ }
 
 function addOption(text, value, selectElement, isDisabled = false) {
-  var option = document.createElement("option");
-  option.text = text;
-  option.value = value;
+   }
 
-  if (isDisabled) {
-    option.disabled = true;
-  }
-
-  selectElement.appendChild(option);
+   selectElement.appendChild(option);
 }
