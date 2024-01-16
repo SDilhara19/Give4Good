083bab786c32e8ec69234f3e16db9fcc1f540a6d
Shaliniamasha <shalakka2002@gmail.com>
2024-01-16 15:42:58 +0530

    hiiii

--- app/controllers/Admin_Fundraisers.php
+++ app/controllers/Admin_Fundraisers.php
@@ -4,31 +4,31 @@ class Admin_Fundraisers extends controller
     private $AdminFundraisersModel;
     public function __construct()
     {
-        $this->AdminStoryModel = $this->model('M_Admin_Fundraiser');
+        $this->AdminFundraisersModel = $this->model('M_Admin_Fundraiser');
   
     }
 
 
     public function index(){
 
-        $data = $this->AdminStoryModel -> getAllFundraisers(); 
+        $data = $this->AdminFundraisersModel -> getAllFundraisers(); 
         
         $this->view('Admin_Fundraisers/V_All', $data);
     }
     public function active(){
-        $data = $this->AdminStoryModel -> viewActive();
+        $data = $this->AdminFundraisersModel -> viewActive();
 
 
         $this->view('Admin_Fundraisers/V_Active', $data);
     }
     public function deactive(){
-        $data = $this->AdminStoryModel -> viewDeactivated();
+        $data = $this->AdminFundraisersModel -> viewDeactivated();
 
         $this->view('Admin_Fundraisers/V_Deact', $data);
     }
     public function pending(){
 
-        $data = $this->AdminStoryModel -> viewPending();
+        $data = $this->AdminFundraisersModel -> viewPending();
 
         $this->view('Admin_Fundraisers/V_Pending' , $data);
     }
