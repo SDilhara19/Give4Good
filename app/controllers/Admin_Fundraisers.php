4c13eb798bcf317016ca400f1541231232b9077b
Shaliniamasha <shalakka2002@gmail.com>
2024-01-16 15:21:58 +0530

    added files

--- app/controllers/Admin_Fundraisers.php
+++ app/controllers/Admin_Fundraisers.php
@@ -1,29 +1,36 @@
 <?php
 class Admin_Fundraisers extends controller
 {
-    // private $prototypeModel;
-    // public function __construct()
-    // {
-    //     $this->prototypeModel = $this->model('M_model')
+    private $AdminFundraisersModel;
+    public function __construct()
+    {
+        $this->AdminStoryModel = $this->model('M_Admin_Fundraiser');
   
-    //}
+    }
 
 
     public function index(){
+
+        $data = $this->AdminStoryModel -> getAllFundraisers(); 
         
-        $this->view('Admin_Fundraisers/V_All');
+        $this->view('Admin_Fundraisers/V_All', $data);
     }
     public function active(){
+        $data = $this->AdminStoryModel -> viewActive();
 
-        $this->view('Admin_Fundraisers/V_Active');
+
+        $this->view('Admin_Fundraisers/V_Active', $data);
     }
     public function deactive(){
+        $data = $this->AdminStoryModel -> viewDeactivated();
 
-        $this->view('Admin_Fundraisers/V_Deact');
+        $this->view('Admin_Fundraisers/V_Deact', $data);
     }
     public function pending(){
 
-        $this->view('Admin_Fundraisers/V_Pending');
+        $data = $this->AdminStoryModel -> viewPending();
+
+        $this->view('Admin_Fundraisers/V_Pending' , $data);
     }
 }
 ?>
