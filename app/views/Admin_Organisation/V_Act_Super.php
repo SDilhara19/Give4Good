44f167be522e5432c696a50aedfb38c932b4c850
SDilhara19 <sdilharabandara@gmail.com>
2024-01-26 22:32:45 +0530

    org header

--- app/views/Admin_Organisation/V_Act_Super.php
+++ app/views/Admin_Organisation/V_Act_Super.php
@@ -15,24 +15,7 @@
 <?php require APPROOT . '/views/includes/admin-dashboard.php' ?>
     
   <div class="main--content">
-    <div class="header--wrapper">
-      <div class="header--title">
-        <i class="fa-solid fa-bars"></i>
-        <span>Donee Organizations (Active)</span>
-      </div>
-      <div class="header--buttons">
-        <a href="?php echo URLROOT?>/Admin_Organisation/index"><button class="all-button">All</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/active"><button class="active-button">Active</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/deactive"><button class="deactivated-button">Deactivated</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/pending"><button class="pending-button">Pending</button></a>
-      </div>
-      <div class="user--info">
-        <div class="search--box">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <input type="text" placeholder="Search Name"/>
-        </div>  
-      </div>   
-    </div>
+  <?php require APPROOT . '/views/Admin_Organisation/Organisation-header.php' ?>
   <div class ="dashDetails">
 <div class="container">
     <div class="table-wrapper">
