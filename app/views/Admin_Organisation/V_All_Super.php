c387e63ed49c6d0116c4f31b8597f16f8b57b3a6
SDilhara19 <sdilharabandara@gmail.com>
2024-01-27 12:33:48 +0530

    admin org details

--- app/views/Admin_Organisation/V_All_Super.php
+++ app/views/Admin_Organisation/V_All_Super.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,114 +8,213 @@
   <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/styles/styles.css">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/styles/components-3/Admin_style.css">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/styles/components/admin.css">
   <script src="https://kit.fontawesome.com/9e9a03ae37.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
   <?php require APPROOT . '/views/includes/admin-header.php' ?>
   <?php require APPROOT . '/views/includes/admin-dashboard.php' ?>
-    
-  <div class="main--content">
-    <div class="header--wrapper">
-      <div class="header--title">
-        <i class="fa-solid fa-bars"></i>
-        <span>Donee Organizations</span>
-      </div>
-      <div class="header--buttons">
-        <a href="?php echo URLROOT?>/Admin_Organisation/index"><button class="all-button">All</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/active"><button class="active-button">Active</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/deactive"><button class="deactivated-button">Deactivated</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/pending"><button class="pending-button">Pending</button></a>
-      </div>
-      <div class="user--info">
-        <div class="search--box">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <input type="text" placeholder="Search Name"/>
-        </div>  
-      </div> 
-     </div>
-     <div class ="dashDetails">
+
+  <div class="main--content admin-organization ">
+    <?php require APPROOT . '/views/Admin_Organisation/Organisation-header.php' ?>
+    <div class="dashDetails">
       <div class="container">
-          <div class="table-wrapper">
-            <table>
-              <thead>
-                <tr>
- 
-                  <th>ID</th>
-                  <th>About</th>
-                  <th>Contact No</th>
-                  <th>Address</th>
-                  <th>Registration Certificate Photo</th>
-                  <th>Logo</th>
-                  <th>Cover Image</th>
-                  <th>Bank Code</th>
-                  <th>Bank Name</th>
-                  <th>Branch Code</th>
-                  <th>Branch Name</th>
-                  <th>Bank Account No</th>
-                  <th>Passbook Photo</th>
-                  <th>Executive Name</th>
-                  <th>Executive  NIC</th>
-                  <th>Executive Address</th>
-                  <th>Executive Contact</th>
-                  <th>Executive Email</th>
-                  <th>Executive NIC Image</th>
-                  <th>Treasurer Name</th>
-                  <th>Treasurer NIC</th>
-                  <th>Treasurer Address</th>
-                  <th>Treasurer Contact</th>
-                  <th>Treasurer Email</th>
-                  <th>Treasurer NIC Image</th>
-                  <th>Status</th>
- 
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <span>
-                      <span class="fi fi-af"></span>
-                    </span>
-                    <span>001</span>
-                  </td>
-                  <td>Non-profit organization</td>
-                  <td>0112458654</td>
-                  <td>No 78, Galle Rd, Dehiwala</td>
-                  <td>regcer.jpg</td>
-                  <td>logo1.jpg</td>
-                  <td>1255</td>
-                  <td>sampath</td>
-                  <td>78</td>
-                  <td>colombo07</td>
- 
-                  <td>7542276464</td>
- 
-                  <td>janithpb.png</td>
-                  <td>1</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
- 
-                  <td>-</td>
-                  <td>Active</td>
-                  <td><button onclick class="data_view">View</button>
-                      <button onclick class="data_deactivate">Deactivate</button>
- 
-                </tr>
-                
-              </tbody>
-            </table>
-          </div> 
+        <div class="table-wrapper">
+          <table class="tablemain">
+            <thead>
+              <tr>
+
+                <th>User ID</th>
+                <th>Username</th>
+                <th>About</th>
+                <th>Contact No</th>
+                <th>Address</th>
+                <th>Registration Certificate Photo</th>
+                <th>Logo</th>
+                <th>Bank Details</th>
+
+                <th>Passbook Photo</th>
+                <th>Executive Details</th>
+                <th>Treasurer Details</th>
+                <th>Status</th>
+
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <span>
+                    <span class="fi fi-af"></span>
+                  </span>
+                  <span>001</span>
+                </td>
+                <td>User1</td>
+                <td>Non-profit organization</td>
+                <td>0112458654</td>
+                <td>No 78, Galle Rd, Dehiwala</td>
+                <td><i class="fa-solid fa-arrow-up-right-from-square"></i></td>
+                <td><i class="fa-solid fa-image" id="open-logo-popup"></i></td>
+                <td><button onclick class="data_view" id="open-bank-popup">View</button></td>
+                <td><i class="fa-solid fa-arrow-up-right-from-square"></i></td>
+                <td><button onclick class="data_view" id="open-executive-popup">View</button></td>
+                <td><button onclick class="data_view" id="open-treasurer-popup">View</button></td>
+                <td>Active</td>
+                <td><button onclick class="data_view">View</button>
+              </tr>
+             
+
+            </tbody>
+          </table>
         </div>
-      </div> 
-      </body>
+
+        <dialog class="popup" id='executive-popup'>
+                <span class="close-popup" id="close-executive-popup">&times;</span>
+                <p class="text-2">Executive: Help us to assist them walk</p>
+                <div class="popup-container nic-container">
+                  <table class="info-table">
+                    <tr>
+                      <th>Name</th>
+                      <td class="text-3">John Doe</td>
+                    </tr>
+                    <tr>
+                      <th>Position</th>
+                      <td class="text-3">Vice-President</td>
+                    </tr>
+
+                    <tr>
+                      <th>NIC No</th>
+                      <td class="text-3">23-02-2024</td>
+                    </tr>
+                    <tr>
+                      <th>Address</th>
+                      <td class="text-3">Ratnapura</td>
+                    </tr>
+                    <tr>
+                      <th>Contact</th>
+                      <td class="text-3">+94 704332324</td>
+                    </tr>
+                    <tr>
+                      <th>Email</th>
+                      <td class="text-3">2johndoe@hmail.com</td>
+                    </tr>
+
+                  </table>
+                  <div class="nic-front">
+
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/NIC-front/1.jpg" alt="">
+                  </div>
+                  <div class="nic-back">
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/NIC-back/1.jpg" alt="">
+                  </div>
+                </div>
+
+              </dialog>
+
+              <dialog class="popup" id='treasurer-popup'>
+                <span class="close-popup" id="close-treasurer-popup">&times;</span>
+                <p class="text-2">Treasurer: Help us to assist them walk</p>
+                <div class="popup-container nic-container">
+                  <table class="info-table">
+                    <tr>
+                      <th>Name</th>
+                      <td class="text-3">John Doe</td>
+                    </tr>
+                    <tr>
+                      <th>Position</th>
+                      <td class="text-3">Treasurer</td>
+                    </tr>
+
+                    <tr>
+                      <th>NIC No</th>
+                      <td class="text-3">23-02-2024</td>
+                    </tr>
+                    <tr>
+                      <th>Address</th>
+                      <td class="text-3">Ratnapura</td>
+                    </tr>
+                    <tr>
+                      <th>Contact</th>
+                      <td class="text-3">+94 704332324</td>
+                    </tr>
+                    <tr>
+                      <th>Email</th>
+                      <td class="text-3">2johndoe@hmail.com</td>
+                    </tr>
+
+                  </table>
+                  <div class="nic-front">
+
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/NIC-front/1.jpg" alt="">
+                  </div>
+                  <div class="nic-back">
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/NIC-back/1.jpg" alt="">
+                  </div>
+                </div>
+
+              </dialog>
+
+              <dialog class="popup" id='bank-popup'>
+                <span class="close-popup" id="close-bank-popup">&times;</span>
+                <p class="text-2">Bank Details: Help us to assist them walk</p>
+                <div class="popup-container bank-container">
+                  <table class="info-table">
+                    <tr>
+                      <th>Bank Name</th>
+                      <td class="text-3">Bank of Ceylon</td>
+                    </tr>
+                    <tr>
+                      <th>Bank Code</th>
+                      <td class="text-3">367</td>
+                    </tr>
+
+                    <tr>
+                      <th>Branch Name</th>
+                      <td class="text-3">Thimbirigasyaya</td>
+                    </tr>
+                    <tr>
+                      <th>Branch Code</th>
+                      <td class="text-3">12</td>
+                    </tr>
+                    <tr>
+                      <th>Account holder</th>
+                      <td class="text-3">Youth Society of Thimbirigasyaya</td>
+                    </tr>
+                    <tr>
+                      <th>Account No</th>
+                      <td class="text-3">5526362</td>
+                    </tr>
+
+                  </table>
+                  <div class="bank-passbook">
+
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/Bank-passbook/1.webp" alt="">
+                  </div>
+                </div>
+
+              </dialog>
+
+              <dialog class="popup-2" id='logo-popup'>
+                <span class="close-popup-2" id="close-logo-popup">&times;</span>
+                <p class="text-2">Logo: Help us to assist them walk</p>
+                <div class="popup-2-container organisation-logo-container">
+                  <div class="organisation-logo">
+                    <img src="<?php echo URLROOT; ?>/Assets/Uploaded-images/Organisation-logo/1.avif" alt="">
+                  </div>
+                </div>
+
+              </dialog>
+
+      </div>
+    </div>
+    <script src="<?php echo URLROOT ?>/public/js/popup.js"></script>
+    <script>
+      setupPopup('#open-executive-popup', '#executive-popup', '#close-executive-popup');
+      setupPopup('#open-treasurer-popup', '#treasurer-popup', '#close-treasurer-popup');
+      setupPopup('#open-bank-popup', '#bank-popup', '#close-bank-popup');
+      setupPopup('#open-logo-popup', '#logo-popup', '#close-logo-popup');
+    </script>
+</body>
+
 </html>
