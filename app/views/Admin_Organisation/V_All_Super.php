4fe938357eb1fbcea20f0f1269180e6432e55979
Sanduni Dilhara <125456493+SDilhara19@users.noreply.github.com>
2024-01-28 13:36:11 +0530

    Update V_All_Super.php

--- app/views/Admin_Organisation/V_All_Super.php
+++ app/views/Admin_Organisation/V_All_Super.php
@@ -15,34 +15,16 @@
   <?php require APPROOT . '/views/includes/admin-header.php' ?>
   <?php require APPROOT . '/views/includes/admin-dashboard.php' ?>
     
-  <div class="main--content">
-    <div class="header--wrapper">
-      <div class="header--title">
-        <i class="fa-solid fa-bars"></i>
-        <span>Donee Organizations</span>
-      </div>
-      <div class="header--buttons">
-        <a href="?php echo URLROOT?>/Admin_Organisation/index"><button class="all-button">All</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/active"><button class="active-button">Active</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/deactive"><button class="deactivated-button">Deactivated</button></a>
-        <a href="?php echo URLROOT?>/Admin_Organisation/pending"><button class="pending-button">Pending</button></a>
-      </div>
-      <div class="user--info">
-        <div class="search--box">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <input type="text" placeholder="Search Name"/>
-        </div>  
-      </div> 
-     </div>
+
+    <div class="main--content admin-organization ">
+     <?php require APPROOT . '/views/Admin_Organisation/Organisation-header.php' ?>
      <div class ="dashDetails">
       <div class="container">
 
       <div class="table-wrapper">
           <table class="tablemain">
             <thead>
-            <?php 
-                 foreach ($data as $super_organisation) {
-                ?>
+           
                 <tr>
 
                 <th>User ID</th>
@@ -61,6 +43,9 @@
               </tr>
             </thead>
             <tbody>
+               <?php 
+                 foreach ($data as $super_organisation) {
+                ?>
               <tr>
                 <td>
                   <!-- <span>
