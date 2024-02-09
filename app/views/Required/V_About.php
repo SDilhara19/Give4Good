36b3e9fc2f5f8c0522d941b21b13c916259aea26
BhagyaLiyanage <bhagyaliyanage99@gmail.com>
2024-02-09 22:51:39 +0530

    Added missed lines

--- app/views/Required/V_About.php
+++ app/views/Required/V_About.php
@@ -16,13 +16,10 @@
 </head>
 
 <body>
-<?php require APPROOT . '/views/includes/side-dash.php' ?>
-
-<!-- <?php //require APPROOT . '/views/includes/header.php' ?> 
-<?php //require APPROOT . '/views/includes/side-dash.php' ?>       
+<?php require APPROOT . '/views/includes/header.php' ?>        
     <main>
-        <h1>About Page</h1>
+        <h1>About Us</h1>
     </main>
-<?php //require APPROOT . '/views/includes/footer.php' ?>  -->
+<?php require APPROOT . '/views/includes/footer.php' ?>  
 </body>
 </html>       
