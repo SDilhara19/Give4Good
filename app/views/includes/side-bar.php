538a6b54fc90e6e8fcd03fff51e77f1c69079589
SDilhara19 <sdilharabandara@gmail.com>
2024-02-12 04:22:57 +0530

    side bar

--- app/views/includes/side-bar.php
+++ app/views/includes/side-bar.php
@@ -1,88 +1,91 @@
 <div class="home-side-bar-main">
     <div class="top-side-bar">
-        <div class="logo-side-bar">
+        <!-- <div class="logo-side-bar">
             <i class="bx bxl-xodepen"></i>
-            <span>CodeCommerce</span>
-        </div>
+            <span>Give4Good</span>
+        </div> -->
         <!-- <i class="bx bx-menu" id="btn"></i> -->
-    </div>
-    <div class="user-side-menu">
-        <img src="<?php echo URLROOT ?>/public/Assets/images/profile.jpeg" alt="me" class="user-img-side-bar">
-        <div>
-            <p class="bold">Client</p>
-            <p>Admin</p>
-        </div>
     </div>
     <ul>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-home bx-sm'></i>
+                <span class="nav-item-side-bar">Home</span>
             </a>
-            <span class="tooltip">Dashxxboard</span>
+            <!-- <span class="tooltip">Dashxxboard</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+                <i class='bx bxs-donate-heart bx-sm'></i>
+                <span class="nav-item-side-bar">Fundraisers</span>
             </a>
-            <span class="tooltip">bbbb</span>
+            <!-- <span class="tooltip">bbbb</span> -->
+        </li>
+        <div class="side-fundraiser-categories">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-hand-holding-heart fa-xl"></i>
+                        <span class="nav-item-side-bar">Fundraisers</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-hand-holding-heart fa-xl"></i>
+                        <span class="nav-item-side-bar">Fundraisers</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fa-solid fa-hand-holding-heart fa-xl"></i>
+                        <span class="nav-item-side-bar">Fundraisers</span>
+                    </a>
+                </li>
+
+            </ul>
+</div>
+        <li>
+            <a href="#">
+                <i class='bx bxs-book-heart bx-sm'></i>
+                <span class="nav-item-side-bar">Stories</span>
+            </a>
+            <!-- <span class="tooltip">Dashboard</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-t-shirt bx-sm'></i>
+                <span class="nav-item-side-bar">Merchandise</span>
             </a>
-            <span class="tooltip">Dashboard</span>
+            <!-- <span class="tooltip">Dashboard</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-caret-up-square bx-sm'></i>
+                <span class="nav-item-side-bar">Start Fundraising</span>
             </a>
-            <span class="tooltip">Dashboard</span>
+            <!-- <span class="tooltip">Create a story</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-star bx-sm'></i>
+                <span class="nav-item-side-bar">Super User</span>
             </a>
-            <span class="tooltip">Dashboard</span>
+            <!-- <span class="tooltip">Dashboard</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-user bx-sm'></i>
+                <span class="nav-item-side-bar">Profile</span>
             </a>
-            <span class="tooltip">Dashboard</span>
+            <!-- <span class="tooltip">Dashboard</span> -->
         </li>
         <li>
             <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
+            <i class='bx bxs-exit bx-sm'></i>
+                <span class="nav-item-side-bar">Logout</span>
             </a>
-            <span class="tooltip">Dashboard</span>
+            <!-- <span class="tooltip">Dashboard</span> -->
         </li>
-        <li>
-            <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
-            </a>
-            <span class="tooltip">Dashboard</span>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
-            </a>
-            <span class="tooltip">Dashboard</span>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bx bxs-grid-alt"></i>
-                <span class="nav-item-side-bar">Dashboard</span>
-            </a>
-            <span class="tooltip">Dashboard</span>
-        </li>
+
     </ul>
 </div>
