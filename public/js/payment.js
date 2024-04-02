2fb5af38bc0d76609f432162ddf1052b15e648d6
SDilhara19 <sdilharabandara@gmail.com>
2024-04-02 23:12:30 +0530

    super mini star, donate

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,49 +1,88 @@
- console.log("PayJS Open")
- // Payment completed. It can be a successful failure.
- payhere.onCompleted = function onCompleted(orderId) {
-    console.log("Payment completed. OrderID:" + orderId);
-    // Note: validate the payment and show success or failure page to the customer
-};
 
-// Payment window closed
-payhere.onDismissed = function onDismissed() {
-    // Note: Prompt user to pay again or show an error page
-    console.log("Payment dismissed");
-};
+function paymentGateway() {
+    console.log("fd")
+    var donationAmount = document.getElementById('donate').value;
+    var contributionAmount = document.getElementById('contribute').value;
 
-// Error occurred
-payhere.onError = function onError(error) {
-    // Note: show an error page
-    console.log("Error:"  + error);
-};
+    // Create a FormData object
 
-// Put the payment variables here
-var payment = {
-    "sandbox": true,
-    "merchant_id": "1226076",    // Replace your Merchant ID
-    "return_url": undefined,     // Important
-    "cancel_url": undefined,     // Important
-    "notify_url": "http://sample.com/notify",
-    "order_id": "2",
-    "items": "Door bell wireles",
-    "amount": "100",
-    "currency": "LKR",
-    "hash": "B44D77A804C4B26D57097CFF134995BD", // *Replace with generated hash retrieved from backend
-    "first_name": "Saman",
-    "last_name": "Perera",
-    "email": "samanp@gmail.com",
-    "phone": "0771234567",
-    "address": "No.1, Galle Road",
-    "city": "Colombo",
-    "country": "Sri Lanka",
-    "delivery_address": "No. 46, Galle road, Kalutara South",
-    "delivery_city": "Kalutara",
-    "delivery_country": "Sri Lanka",
-    "custom_1": "",
-    "custom_2": ""
-};
+    var formData = new FormData();
+    formData.append('donationAmount', donationAmount);
+    formData.append('contributionAmount', contributionAmount);
 
-// Show the payhere.js popup, when "PayHere Pay" is clicked
-document.getElementById('payhere-payment').onclick = function (e) {
-    payhere.startPayment(payment);
-};
+    console.log("Doonate Amount:", donationAmount);
+    console.log("Contribute Amount:", contributionAmount);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {
+
+
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            alert(xhttp.responseText);
+            var obj = JSON.parse(xhttp.responseText);
+
+            console.log("PayJS Open")
+            // Payment completed. It can be a successful failure.
+            payhere.onCompleted = function onCompleted(orderId) {
+                console.log("Payment completed. OrderID:" + orderId);
+                // Note: validate the payment and show success or failure page to the customer
+            };
+
+            // Payment window closed
+            payhere.onDismissed = function onDismissed() {
+                // Note: Prompt user to pay again or show an error page
+                console.log("Payment dismissed");
+            };
+
+            // Error occurred
+            payhere.onError = function onError(error) {
+                // Note: show an error page
+                console.log("Error:" + error);
+            };
+
+
+
+
+            // Put the payment variables here
+            var payment = {
+                "sandbox": true,
+                "merchant_id": "1226076",    // Replace your Merchant ID
+                "return_url": "http://localhost/give4good/Donate/paydone",     // Important
+                "cancel_url": "http://localhost/give4good/Donate/paydone",     // Important
+                "notify_url": "http://localhost/give4good/Donate/paydone",
+                "order_id": obj["order_id"],
+                "items": "Door bell wxireles",
+                "amount": obj["amount"],
+                "currency": obj["currency"],
+                "hash": obj["hash"], // *Replace with generated hash retrieved from backend
+                "first_name": "Saman",
+                "last_name": "Perera",
+                "email": "samanp@gmail.com",
+                "phone": "0771234567",
+                "address": "No.1, Galle Road",
+                "city": "Colombo",
+                "country": "Sri Lanka",
+                "delivery_address": "No. 46, Galle road, Kalutara South",
+                "delivery_city": "Kalutara",
+                "delivery_country": "Sri Lanka",
+                "custom_1": "",
+                "custom_2": "",
+                "donateAmount": obj["donateAmount"],
+                "contributeAmount": obj["contributeAmount"],
+
+            };
+            payhere.startPayment(payment);
+        }
+    }
+    xhttp.open("POST", "http://localhost/give4good/Donate/payG", true);
+    xhttp.send(formData);
+}
+// // Call the paymentGateway function with the stored values
+// document.getElementById("donateBtn").addEventListener("click", function() {
+//   paymentGateway(donateAmount, contributeAmount);
+// });
+
+
+
+
+
+
